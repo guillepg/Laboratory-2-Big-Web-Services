8c1fb045b08931e6fd31fca183e77b847938799e
guillepg <guillepg@outlook.es>
2015-10-29 10:10:36 +0100

    build 10

--- src/test/java/translator/web/ws/TranslatorEndpointTest.java
+++ src/test/java/translator/web/ws/TranslatorEndpointTest.java
@@ -42,10 +42,10 @@ public class TranslatorEndpointTest {
 		GetTranslationRequest request = new GetTranslationRequest();
 		request.setLangFrom("en");
 		request.setLangTo("es");
-		request.setText("This is a test of translation service");
+		request.setText("Hello");
 		Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"+ port + "/ws", request);
 		assertNotNull(response);
 		assertThat(response, instanceOf(GetTranslationResponse.class));
-		assertThat(((GetTranslationResponse) response).getTranslation(), is("Esto es una prueba de servicio de traducción"));
+		assertThat(((GetTranslationResponse) response).getTranslation(), is("Hola"));
 	}
 }
